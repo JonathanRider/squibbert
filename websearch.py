3a2873aa6bfb7ca9e614d168e39375062e6aafe4
Jonathan Rider <slith93@gmail.com>
2019-06-27 22:30:56 -0400

    Specify site type

--- websearch.py
+++ websearch.py
@@ -2,12 +2,15 @@ import googlesearch
 import webbrowser
 import sys
 
-query = sys.argv[1]
+site_type = sys.argv[1]
+query = sys.argv[2]
 
-print(query)
 
-test = query +  " site:sec.gov"
-print(test)
-for url in googlesearch.search(test, stop=5):
-    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-    webbrowser.get(chrome_path).open(url)
+site_types = {"health": ["sec.gov", 'healthcare.gov'],
+            "example": ["yahoo.com"]
+        }
+
+for item in site_types[site_type]:
+    for url in googlesearch.search(item + query, stop=5):
+        chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open(url)
